6f96e65c50881a0e037d7bc23a340afea0530a4b
annatyler <anna.l.tyler@gmail.com>
2020-10-15 10:17:00 -0400

    Update 1.1.bedtools.sh

--- Documents/1.Setup/1.1.bedtools.sh
+++ Documents/1.Setup/1.1.bedtools.sh
@@ -2,13 +2,12 @@
 
 module load singularity
 
-cd /projects/carter-lab/hepatocytes/ChromHMM/data/bamfiles
-
+inputdir=$/projects/carter-lab/hepatocytes/ChromHMM/data/bamfiles
 targetdir=$/projects/carter-lab/hepatocytes/ChromHMM/data/bedfiles
 
 for x in *.sorted.B6co.bam ; do
     echo "print current:$x";
     #echo "$targetdir${x%.bam}.bed";
-    bedtools bamToBed -i "$x" > "$targetdir${x%.bam}.bed";
+    singularity exec ../Containers/bedtools_v2.27.1dfsg-4-deb_cv1.sif 'bedtools bamToBed -i' "$x" > "$targetdir${x%.bam}.bed";
 done
 echo "done"
