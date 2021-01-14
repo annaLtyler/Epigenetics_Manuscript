aefb141c004f9c9ce294e2b3ebf1f30c9603c704
annatyler <anna.l.tyler@gmail.com>
2021-01-14 12:53:27 -0500

    updated eQTL code to get both LOD scores and coefficients

--- Code/epigenetics/get.cis.coef.R
+++ Code/epigenetics/get.cis.coef.R
@@ -1,19 +1,20 @@
 #This function gets the cis coefficient
 #from a gene given its index in do.eqtl
 #from epigen.states.and.expr
-get.cis.coef <- function(do.eqtl, index){
+get.cis.coef <- function(do.eqtl, mrna.info, index){
 
-  	idx.info <- do.eqtl[[index]]
-	coef.list <- idx.info$DO.coefficients
-	if(!is.null(coef.list)){
-		coords <- as.numeric(unlist(lapply(strsplit(rownames(coef.list), "_"), function(x) x[2])))
-  		gene.pos <- mean(c(idx.info[[1]]$Start.Mbp, idx.info[[1]]$End.Mbp)) * 1e6
-		closest.idx <- which.min(abs(coords - gene.pos))
-  		cis.coef <- idx.info$DO.coefficients[closest.idx,]
-	  	# plot(coords, idx.info[[3]], type = "l")
-  		# abline(v = gene.pos)
-	  	return(cis.coef)	
-	  	}else{
-  		return(rep(0, ncol(do.eqtl[[1]]$DO.coefficients)))
-	  	}
+	if(is.null(do.eqtl[[index]])){
+		return(rep(NA, 3))
+	}
+
+  	transcript.id <- names(do.eqtl)[index]
+	nearest_marker <- unlist(mrna.info[index,"nearest.marker.id"])
+	if(!is.null(nearest_marker) && !is.na(nearest_marker)){
+		marker_idx <- which(rownames(do.eqtl[[index]]) == nearest_marker)
+		cis.coef <- do.eqtl[[index]][marker_idx]
+		result <- c(transcript.id, nearest_marker, cis.coef)
+	}else{
+		result <- c(transcript.id, nearest_marker, NA)
+	}
+	return(result)
   	}
